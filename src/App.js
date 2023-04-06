8942be717cf2aee0f4844ad5388f4e19bffcc631
root <root@Robix>
2023-04-06 05:02:29 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import CategoryDirectory from './components/category-directory/category-directory.component';
+
+const App = () => {
+
+    return (
+        <CategoryDirectory/>
+    );
+}
+
+export default App;
